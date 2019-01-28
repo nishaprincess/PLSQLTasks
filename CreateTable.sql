de8e1c80b7119742178f2499661b8de5760bad54
nishaprincess <rajashreem52@gmail.com>
2019-01-28 21:02:13 +0600

    first commit

---
+++ CreateTable.sql
@@ -0,0 +1,12 @@
+ 
+ 
+
+Begin
+Create table Student2
+(
+ id int,
+ st_name varchar(50),
+ adress varchar(60)
+
+);
+End;
---
+++ insert.sql
@@ -0,0 +1,5 @@
+Create procedure ins_Student
+AS
+BEGIN 
+insert into dbo.Student2 values(3,'sumu','Rampura');
+END;
